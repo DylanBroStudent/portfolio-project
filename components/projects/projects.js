47665e3da70d52db514715e8c80e9e7e5b7d9b2a
Dylan <dylan-brodie@live.nmit.ac.nz>
2025-11-02 20:21:30 +1300

    comment section formatting

--- components/projects/projects.js
+++
@@ -1,9 +0,0 @@
-// Import load component
-import { loadComponent } from "/components/component-manager.js";
-
-// wait for DOM to load
-document.addEventListener("DOMContentLoaded", () => {
-    // Populate project page with content
-    // Comment section
-    loadComponent("comment-section-placeholder", "/components/comment-section/comment-section.html");
-});
