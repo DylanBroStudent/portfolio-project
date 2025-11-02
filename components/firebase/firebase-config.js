0377b62bbefe1e3ef24e04a3218e26464222b9e8
Dylan <dylan-brodie@live.nmit.ac.nz>
2025-11-02 15:20:39 +1300

    firebase setup

---
+++ components/firebase/firebase-config.js
@@ -0,0 +1,22 @@
+// use dotenv to get environment variables
+require('dotenv').config();
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Firebase configuration object
+const firebaseConfig = {
+  apiKey: process.env.FB_apiKey,
+  authDomain: process.env.FB_authDomain,
+  projectId: process.env.FB_projectId,
+  storageBucket: process.env.FB_storageBucket,
+  messagingSenderId: process.env.FB_messagingSenderId,
+  appId: process.env.FB_appId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
